cc344587ac42d1b242ecd7a8bd0e34ad27a61263
toviChervonitz <tovichervonitz1234@gmail.com>
2026-01-08 08:59:51 +0200

    Update app.php

--- apps/backend/bootstrap/app.php
+++ apps/backend/bootstrap/app.php
@@ -1,9 +1,17 @@
 <?php
 
+use App\Http\Middleware\AdminMiddleware;
+use App\Http\Middleware\Authenticate;
+use App\Http\Middleware\CookieTokenToBearer;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Auth\AuthenticationException;
+use Laravel\Sanctum\Http\Middleware\CheckForAnyScope;
+use Laravel\Sanctum\Http\Middleware\CheckScopes;
+use Spatie\Permission\Middleware\PermissionMiddleware;
+use Spatie\Permission\Middleware\RoleMiddleware;
+use Spatie\Permission\Middleware\RoleOrPermissionMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,24 +20,27 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
-   ->withMiddleware(function (Middleware $middleware): void {
-    $middleware->throttleApi('60,1');
+    ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->throttleApi('60,1');
 
-    $middleware->priority([
-        \App\Http\Middleware\CookieTokenToBearer::class,
-        \App\Http\Middleware\Authenticate::class,
-        \Illuminate\Routing\Middleware\SubstituteBindings::class,
-    ]);
+        $middleware->priority([
+            CookieTokenToBearer::class,
+            Authenticate::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ]);
 
-    $middleware->alias([
-        'auth' => \App\Http\Middleware\Authenticate::class,
-        'cookieAuth' => \App\Http\Middleware\CookieTokenToBearer::class,
-        'admin' => \App\Http\Middleware\AdminMiddleware::class,    
-        'scopes' => \Laravel\Passport\Http\Middleware\CheckScopes::class,
-        'scope' => \Laravel\Passport\Http\Middleware\CheckForAnyScope::class,
-   
-        
-    ]);
+        $middleware->alias([
+            'auth' => Authenticate::class,
+            'cookieAuth' => CookieTokenToBearer::class,
+            'admin' => AdminMiddleware::class,
+            'scopes' => CheckScopes::class,
+            'scope' => CheckForAnyScope::class,
+
+            'permission' => PermissionMiddleware::class,
+            'role' => RoleMiddleware::class,
+            'role_or_permission' => RoleOrPermissionMiddleware::class,
+
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
 
